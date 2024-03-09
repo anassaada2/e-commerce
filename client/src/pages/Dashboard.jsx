a26232a0278feacc567d54e5035f2e41f57c4ed4
anassaada2 <anassaada71@gmail.com>
2024-03-09 21:15:37 +0100

    complete user image upload functionality

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -19,7 +19,7 @@ if(tabFromUrl){
         {/*sidbar */}
         <DashSidebar/>
            </div>
-      <div className='mx-auto'>
+      <div className=' w-full '>
         {/* right side */}
         {tab === 'profile' && <DashProfile/>}
       </div>
