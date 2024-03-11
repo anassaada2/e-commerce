bd330fd1d47e6afbfd3801a33586ff15f278be1e
anassaada2 <anassaada71@gmail.com>
2024-03-11 01:50:36 +0100

    add update user api route

--- api/index.js
+++ api/index.js
@@ -3,12 +3,14 @@ import mongoose from 'mongoose'
 import userRoutes from './routes/user.route.js'
 import dotenv from 'dotenv'
 import authRoutes from './routes/auth.routes.js'
+import cookieParser from 'cookie-parser'
 dotenv.config()
 mongoose.connect(process.env.MONGODB)
 .then(()=>{console.log('Mongodb connected')})
 .catch((err)=>{console.log(err)})
 const app = express();
 app.use(express.json());
+app.use(cookieParser())
 app.listen(3000,()=>{console.log('server is running on port 3000')});
 app.use('/api/user',userRoutes);
 app.use('/api/auth',authRoutes);
